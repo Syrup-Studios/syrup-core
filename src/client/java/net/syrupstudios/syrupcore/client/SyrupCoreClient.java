3f4f4ce1c74f1790a7474401da781b10597540be
Salad <salad@bysalad.studio>
2026-05-25 17:04:20 +0200

    init

---
+++ src/client/java/net/syrupstudios/syrupcore/client/SyrupCoreClient.java
@@ -0,0 +1,10 @@
+package net.syrupstudios.syrupcore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SyrupCoreClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
